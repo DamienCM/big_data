db27fd3223dca70ad19ef1dc5b092930b637d046
DamienCM <45606435+DamienCM@users.noreply.github.com>
2021-01-18 14:52:40 +0100

    modification gen.py
    visualisation de la distribution

--- python/generation_fichier/gen.py
+++ python/generation_fichier/gen.py
@@ -1,7 +1,8 @@
 import numpy as np
 import time 
 from datetime import datetime
-
+import matplotlib.pyplot as plt
+import pandas as pd
 
 t0=time.time()
 
@@ -10,16 +11,23 @@ path='./data.csv'
 taille_de_fichier = 10_000
 
 O3=np.random.uniform(low=0,high=1,size=taille_de_fichier)
-NO2=np.random.binomial(0,1,size=taille_de_fichier) # Parameters : -n int or array_like of ints Parameter of the distribution, 
-                                        #              >= 0. Floats are also accepted, but they will be truncated to integers. 
-                                        #              
-										# 	 		   -p float or array_like of floats
-                                        # 				Parameter of the distribution, >= 0 and <=1.
+NO2=np.random.normal(loc=.5, scale=1,size=taille_de_fichier) 
 
-SO2=np.random.gamma(0,scale=1,size=taille_de_fichier) #Parameters shape  : float or array_like of floats ; The shape of the gamma distribution. Must be non-negative.
+SO2=np.random.gamma(0.5,scale=1,size=taille_de_fichier) #Parameters shape  : float or array_like of floats ; The shape of the gamma distribution. Must be non-negative.
 CO=np.random.exponential(scale=1,size=taille_de_fichier)
 
 
+#Visualisation des distributions
+
+fig,axes=plt.subplots(4,1)
+fig.set_size_inches(16,9)
+data=[O3,NO2,SO2,CO]
+labels=['O3','NO2','SO2','CO']
+
+for i in range(len(labels)):
+    axes[i].hist(data[i],bins=200,label=labels[i])
+    axes[i].legend()
+fig.savefig('distribution.png',dpi=800)
 
 np.savetxt(path,np.transpose([O3,NO2,SO2,CO]),delimiter=',',header=f'File generated on : {datetime.now()} by damien-mouad \n O3,NO2,SO2,CO')
 
