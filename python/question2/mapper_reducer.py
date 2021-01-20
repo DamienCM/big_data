6cf8165a3766823615e4437c61790c56a006589d
DamienCM <45606435+DamienCM@users.noreply.github.com>
2021-01-20 10:03:52 +0100

    question 2

---
+++ python/question2/mapper_reducer.py
@@ -0,0 +1,47 @@
+from mrjob.job import MRJob, MRStep
+import time
+
+# On cherche ici a calculer le nombre d'occurence ou sur une ligne il y a des combinaisons
+# telles que la somme de 2 facteurs est superieure a 0.5
+# on renverra le nombre de combinaison qui donne un resultat > 0.5 pour chaque element
+#  ex : 
+# C02 : 40 (sur les lignes il y a 40 combinaisons avec les autres colonne de CO2 qui donne un resultat superieur a 0.5)
+# SO : 200
+# etc
+
+class Mapper(MRJob):
+
+    def filter(self, key, ligne):
+        els_ligne = ligne.split(',') #recuperation des differentes mesures d'une ligne
+        keys = ['O3', 'NO2', 'SO2', 'CO'] #cles valeurs des colonnes 
+
+        try : 
+            valeurs = [float(v) for v in els_ligne] # convertit [floats] <-- [strings]
+            counts = []
+            for i1,v1 in enumerate(valeurs): #calcule le nombre de combinaison presente sur la ligne pour la i eme valeur
+                count = 0 #nombre de combinaison init= 0
+                for i2,v2 in enumerate(valeurs): #compare avec les autres valeurs de la ligne
+                    if (v1 + v2) > 0.5 and i1 != i2 :
+                        count+=1
+                counts.append(count)
+            yield "Combinaisons", counts #renvoie le nombre de combinaison 
+
+        except ValueError:
+            pass
+
+    def reducer(self, key, values):
+        sums = next(values) #initialisation de la somme
+        for line in values: 
+            for i in range(len(sums)): #parcoure les lignes
+                sums[i]+=line[i]
+        yield key, sums #retourne pour chaque cle le nombre de combianison
+
+
+    def steps(self):
+        return [MRStep(mapper=self.filter),MRStep(reducer=self.reducer)]
+
+
+if __name__ == "__main__":
+    t0 = time.time()
+    Mapper.run()
+    print(f'Program executed in {time.time() - t0} seconds')
--- python/question3/mapper_reducer.py
+++ python/question3/mapper_reducer.py
@@ -1,35 +1,33 @@
 from mrjob.job import MRJob, MRStep
 import time
 
-maximum = 0
-minimum = 0
-
-#keys_val = ['O3_val', 'NO2_val', 'SO2_val', 'CO_val']
-
 class Mapper(MRJob):
+
     def filter(self, key, ligne):
-        el_ligne = ligne.split(',')
-        keys = ['O3', 'NO2', 'SO2', 'CO']
+        el_ligne = ligne.split(',') #recuperation des differentes mesures d'une ligne
+        keys = ['O3', 'NO2', 'SO2', 'CO'] #cles valeurs des colonnes 
+
         for i, element in enumerate(el_ligne):
             try:
                 temp = float(element)
-                if temp > 0.5:
+                if temp > 0.5 :  # test de si la mersure est valide
                     yield (keys[i], (temp, 1))
-                    #yield (keys_val[i], temp)
-            except ValueError:
+
+            except ValueError: #traitement du cas ou une ligne n'est pas lisible (header/footer/erreur d'ecriture)
                 pass
+
+
     def reducer(self, key, values):
-        min_val = next(values)
-        max_val = min_val
-        count = 0
-        avg = 0
+        min_val = next(values) #valeurs initiale
+        max_val = min_val      #valeur initiale
+        count = 0              #valeur initiale
+        avg = 0                #valeur initiale
         for element in values:
             avg += element[0]
-            min_val = min(element, min_val)
-            max_val = max(element, max_val)
+            min_val = min(element, min_val) #compare la valeur actuelle avec la valeur iteree  
+            max_val = max(element, max_val) #compare la valeur actuelle avec la valeur iteree 
             count += 1
-        yield key, (min_val[0], max_val[0], count, avg/count)
-
+        yield key, (min_val[0], max_val[0], count, avg/count) #retourne pour chaque cle : min, max, nombre de mesure valide, moyenne des mesures valide
 
 
     def steps(self):
@@ -37,6 +35,6 @@ class Mapper(MRJob):
 
 
 if __name__ == "__main__":
-    t0 = time.time()
+    t0 = time.time() #calcul de temps
     Mapper.run()
-    print(f'Program executed in {time.time() - t0} seconds')
+    print(f'Program executed in {time.time() - t0} seconds') #calcul de temps
