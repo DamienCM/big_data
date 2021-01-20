bd8195a2e1cdcf6a0bfc3d2d13768f5ba27afb17
mouad abrini <mouad.abrini@gmail.com>
2021-01-20 23:42:00 +0100

    dernier push

--- python/question1/mapper_reducer.py
+++ python/question1/mapper_reducer.py
@@ -2,22 +2,22 @@ from mrjob.job import MRJob, MRStep
 
 
 class Mapper(MRJob):
-    def filter(self, key, ligne):
-        el_ligne = ligne.split(',')
-        keys = ['O3','NO2','SO2','CO']
+    def mapper(self, key, ligne):
+        el_ligne = ligne.split(',')#recuperation des differentes mesures d'une ligne
+        keys = ['O3','NO2','SO2','CO'] #cles valeurs des colonnes
         for i, element in enumerate(el_ligne):
             try:
-                if float(element) > 0.5:
+                if float(element) > 0.5:# test de si la mersure est valide
                     yield (keys[i], 1)
             except ValueError:
                 pass
-    def reducer(self, key, values):
-        yield(key, sum(values))
+    def reducer(self, key, values):#traitement du cas ou une ligne n'est pas lisible (header/footer/erreur d'ecriture)
+        yield(key, sum(values)) #On calcule la somme des mesures valide
 
 
 
     def steps(self):
-        return [MRStep(mapper=self.filter), MRStep(reducer=self.reducer)]
+        return [MRStep(mapper=self.mapper), MRStep(reducer=self.reducer)]
 
 
 if __name__ == "__main__":
