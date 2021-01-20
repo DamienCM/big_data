8fa0357712c3d24ea881997c356cd0c244969608
DamienCM <45606435+DamienCM@users.noreply.github.com>
2021-01-20 14:27:40 +0100

    generation mieux pensee pour la memoire pc
    il y a plus le header par contre

--- python/generation_fichier/gen.py
+++ python/generation_fichier/gen.py
@@ -3,32 +3,42 @@ import time
 from datetime import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
+import os 
+
 
 t0=time.time()
 
 path='./data.csv'
 
-taille_de_fichier = 10_000
+nombre_de_kilo_ligne = 5000
 
-O3=np.random.uniform(low=0,high=1,size=taille_de_fichier)
-NO2=np.random.normal(loc=.5, scale=1,size=taille_de_fichier) 
+taille_batch = 1_000
 
-SO2=np.random.gamma(0.5,scale=1,size=taille_de_fichier) #Parameters shape  : float or array_like of floats ; The shape of the gamma distribution. Must be non-negative.
-CO=np.random.exponential(scale=1,size=taille_de_fichier)
+file = open(path, 'wb')
+
+for i in  range(nombre_de_kilo_ligne):
+    if i%100==0:
+        print(f"Progression : {100*float(i/nombre_de_kilo_ligne)} %" )
+    O3=np.random.uniform(low=0,high=1,size=taille_batch)
+    NO2=np.random.normal(loc=.5, scale=1,size=taille_batch) 
+
+    SO2=np.random.gamma(0.5,scale=1,size=taille_batch) #Parameters shape  : float or array_like of floats ; The shape of the gamma distribution. Must be non-negative.
+    CO=np.random.exponential(scale=1,size=taille_batch)
 
 
-#Visualisation des distributions
+    #Visualisation des distributions
 
-"""fig,axes=plt.subplots(4,1)
-fig.set_size_inches(16,9)
-data=[O3,NO2,SO2,CO]
-labels=['O3','NO2','SO2','CO']
+    """fig,axes=plt.subplots(4,1)
+    fig.set_size_inches(16,9)
+    data=[O3,NO2,SO2,CO]
+    labels=['O3','NO2','SO2','CO']
 
-for i in range(len(labels)):
-    axes[i].hist(data[i],bins=200,label=labels[i])
-    axes[i].legend()
-fig.savefig('distribution.png',dpi=800)"""
+    for i in range(len(labels)):
+        axes[i].hist(data[i],bins=200,label=labels[i])
+        axes[i].legend()
+    fig.savefig('distribution.png',dpi=800)"""
 
-np.savetxt(path,np.transpose([O3,NO2,SO2,CO]),delimiter=',',header=f'File generated on : {datetime.now()} by damien-mouad \n O3,NO2,SO2,CO')
+    np.savetxt(file,np.transpose([O3,NO2,SO2,CO]),delimiter=',')
 
+file.close()
 print(f'Program executed in {time.time()-t0} seconds')
